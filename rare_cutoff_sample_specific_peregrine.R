fdff10bc8b7551bad19b150ab39ed3ef25804cee
Xiu Jia <xibeihenai@gmail.com>
2019-10-31 16:42:22 +0100

    Add files via upload

---
+++ rare_cutoff_sample_specific_peregrine.R
@@ -0,0 +1,70 @@
+# Using sample-specific rarity cutoffs to define the rare and common biopsheres
+# Author: Xiu Jia
+# Date: 09-04-2019
+
+# load library
+library(vegan)
+
+# load feature table
+com <- read.csv("schier_cdna_feature_table_dada2_silva_rarefied_taxonomy.csv", sep=",",  header=1, row.names=1)
+com <- t(com[, 1:60])
+str(com)
+
+# calculate Chao1 for estimation of the sequencing depth
+# to be notice, Chao1 could only represent the lower bundary of richness estimation
+Chao <- as.data.frame(t(estimateR(com)))
+Chao$slope <- Chao$S.obs/Chao$S.chao1
+head(Chao)
+
+
+# built a empty matrix to store the sample-specific rarity cutoffs
+cutoffs <- matrix(NA, nrow(com), 3)
+row.names(cutoffs) <- row.names(com)
+cutoffs[,2] <- Chao$slope
+cutoffs[,3] <- Chao$S.obs
+colnames(cutoffs) <- c("Rarity.cutoffs", "Slopes", "S.obs")
+
+# using method calculate H-index to calculate rarity cutoff per sample
+for (j in 1:nrow(com)) {
+  com_j = sort(as.numeric(com[j,]), decreasing = TRUE)
+  com_j <- com_j[com_j!=0]
+  slope <- cutoffs[j,2]
+  for (i in 1:length(com_j)){
+    if (com_j[i]>=i*slope){
+      H=i
+    }
+  }
+  cutoffs[j, 1] <- H
+}
+cutoffs <- as.data.frame(cutoffs)
+head(cutoffs)
+
+
+# generate the dataset of the rare biosphere
+df <- com
+for (j in 1:nrow(df)) {
+  for (i in 1:ncol(df)) {
+    if (df[j, i] > cutoffs[j,1]) {
+      df[j, i] <- NA
+    }
+  }
+}
+
+df[is.na(df)] <- 0
+df <- df[, colSums(df)!=0] 
+df <- df[, rowSums(df)!=0] 
+write.csv(t(df), "schier_rare_specitic_cutoffs.csv")
+
+# generate the dataset of the common biosphere
+df <- com
+for (j in 1:nrow(df)) {
+  for (i in 1:ncol(df))
+    if (df[j, i] <= cutoffs[j,1]) {
+      df[j,i] <- NA
+    }
+}
+
+df[is.na(df)] <- 0
+df <- df[, colSums(df)!=0] 
+df <- df[, rowSums(df)!=0] 
+write.csv(t(df), "schier_common_specitic_cutoffs.csv")

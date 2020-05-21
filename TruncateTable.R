e79a0292d1c3214bd45883f7d65cbf2821ec3c00
Xiu Jia <xibeihenai@gmail.com>
2020-05-21 17:55:50 +0200

    Add files via upload

---
+++ TruncateTable.R
@@ -0,0 +1,27 @@
+# Based on Angelique Gobet & Alban Ramette, MultiCoLA Manual - Quick and Easy version (2011) 
+# https://www.mpi-bremen.de/Binaries/Binary1660/MultiCoLA.1.4.-.zip
+# Modified by Xiu Jia at University of Groningen on 01-09-2018
+
+# sample-based cutoff (Gobet at al., 2010)
+TruncateTable<-function(dataset,cutoff,typem){
+        # remove columns in the matrix for which the sum of the line is 0
+        CLrow<-function(m) {
+          m <- m[, colSums(m)!=0] 
+          return(m)
+         }
+
+        CLcol<-function(m) {
+          # remove all the rows in the matrix for which the sum of the line is 0
+          # the case happend only if you increased the cutoff untill it reaches the lowest number of the maximum OTU occurance in all samples
+          m <- m[, rowSums(m)!=0]
+          return (m)
+         }
+
+			# Application of a percentage cut-off to the original dataset to obtain abundant/rare dataset
+				Q1<-dataset
+				if(typem == "dominant") {Q1[Q1/rowSums(Q1) <= cutoff] <- 0}	# all species presents no less than j times =0
+				if(typem == "rare") {Q1[Q1/rowSums(Q1) > cutoff] <- 0}	# all species presents more than j times =0
+				Q3 <- CLcol(CLrow(Q1))	#remove rows and columns whose sum=0
+return(Q3)
+} #end TruncateTable
+
